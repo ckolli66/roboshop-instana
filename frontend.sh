fab4cd27e9171043e67c5cf51fd06732e02fe7d0
Chanu <kollichanu@gmail.com>
2025-11-21 20:20:31 -0600

    Updated Frontend Script

--- frontend.sh
+++ frontend.sh
@@ -5,14 +5,13 @@ dnf install nginx -y
 
 #Removing any nginx server content before versions have
 rm -rf /usr/share/nginx/html/*
+cp nginx.conf /etc/nginx/nginx.conf
 
 # shellcheck disable=SC2164
 cd /usr/share/nginx/html #Moving to nginx server html directory
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip #Getting the frontend code
 
 unzip /tmp/frontend.zip # extracting frontend code
-
-cp nginx.conf /etc/nginx/nginx.conf
 
 systemctl enable nginx
 systemctl restart nginx
