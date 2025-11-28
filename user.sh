e2656155cf67408c68d33bcdc4e4d7835136471a
Chanu <kollichanu@gmail.com>
2025-11-28 17:18:25 -0600

    modified user.service and user.sh

--- user.sh
+++ user.sh
@@ -4,4 +4,18 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 
 cp user.service /etc/systemd/system/user.service
-cp 
+useradd roboshop
+
+mkdir /app
+# shellcheck disable=SC2164
+cd /app
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
+unzip /tmp/user.zip
+
+npm install
+
+systemctl daemon-reload
+
+systemctl enable user
+systemctl restart user
