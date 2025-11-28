3bb6b3775b247708e044f8a41f3288e406065c17
Chanu <kollichanu@gmail.com>
2025-11-28 17:04:04 -0600

    renamed catalogue.service

--- catalogue.sh
+++ catalogue.sh
@@ -20,7 +20,7 @@ npm install
 
 systemctl daemon-reload
 dnf install mongodb-mongosh -y
-mongosh --host mongodb-dev.chanakyakolli.online </app/db/master-data.js
+mongosh --host 100.28.228.217 </app/db/master-data.js
 
 systemctl enable catalogue
 systemctl restart catalogue
